fbf4c22e32ba8110c1d21457c7c6270b87116887
Tomislav Stamenovski <krypton.style@gmail.com>
2018-04-09 20:38:07 +0000

    automatically backed up by learn

--- other_file.js
+++ other_file.js
@@ -2,8 +2,10 @@ var index = require("./index.js")
 
 if (index.name === "Susan") {
   console.log("The name is correct");
-} else if {
+}
 
+if (index.height === 70) {
+  console.log();
 }
 console.log(index.name)
 console.log(index.height);
